835a00b77c19ecfdc51d83582b8bfe8840a4d539
BaddCamden <84610525+Camden-Tech@users.noreply.github.com>
2024-01-11 12:51:06 -0600

    Update World.cs
    World Generation Method GenerateChunk fleshed out

--- Onyxalis/Objects/Worlds/World.cs
+++ Onyxalis/Objects/Worlds/World.cs
@@ -14,11 +14,13 @@ namespace Onyxalis.Objects.Worlds
         public Weather weather;
         public int seed;
         public Random worldRandom;
+
+        
         public static World CreateWorld()
         {
             World world = new World();
             world.GenerateSeed();
-            world.weather = new Weather();
+            world.weather = new Weather(); //Generate Weather Method
             return world;
         }
 
@@ -27,6 +29,20 @@ namespace Onyxalis.Objects.Worlds
         {
             seed = Game1.GameRandom.Next();
             worldRandom = new Random(seed);
+        }
+
+
+        public Chunk GenerateChunk(int x, int y)
+        {
+            Chunk newChunk = chunks[x][y];
+            if(newChunk != null){
+                newChunk.GenerateTiles();
+            } else {
+                newChunk = Chunk.CreateChunk(true); //Create CreateChunk() method, the boolean decides whether the chunk generates terrain or not.
+            }
+            
+            chunks[x][y] = newChunk;
+            return newChunk;
         }
 
         public Vector2 SpawnPlayerIn()
