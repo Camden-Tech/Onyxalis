03650424040e6b0b705eb7d26bed3cd5e1250f8e
Blue Robin <julianjohnt@gmail.com>
2024-01-13 20:27:46 -0600

    lol

--- Onyxalis/Objects/hitboxes.cs
+++ Onyxalis/Objects/hitboxes.cs
@@ -9,14 +9,17 @@ namespace Onyxalis.Objects
 {
      public class Hitbox
     {
-        public (int x, int y) topRight = (10, 0);
-        public (int x, int y) topLeft = (0, 0);
-        public (int x, int y) bottomRight = (10, 10);
-        public (int x, int y) bottomLeft = (0, 10);
+        public (int x, int y) topRight;
+        public (int x, int y) topLeft;
+        public (int x, int y) bottomRight;
+        public (int x, int y) bottomLeft;
 
         public Hitbox(int sizeX, int sizeY)
         {
-
+            topRight = (sizeX, 0);
+            topLeft = (0, 0);
+            bottomRight = (sizeX, sizeY);
+            bottomLeft = (0, sizeY);
         }
     }
 }
